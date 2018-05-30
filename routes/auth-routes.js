b3dc8a978bd37c3ba3ae3b0c4ccb380afbdbc899
Richard Batista <stalfos@icloud.com>
2018-05-30 18:51:04 -0400

    Minor clean up

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,20 +3,12 @@ const authRoutes = express.Router();
 const passport = require("passport");
 // User model
 const User = require("../models/user");
-
 const flash = require("connect-flash");
-
 const ensureLogin = require("connect-ensure-login");
-
-
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcryptjs");
 const bcryptSalt = 10;
-
-// authRoutes.get("/signup", (req, res, next) => {
-//   res.render("auth/signup");
-// });
 
 authRoutes.post("/signup", (req, res, next) => {
   const username = req.body.username;
